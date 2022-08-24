99b4db81706c349eb6a2993ed80932bb2dd0ab63
Deepansh-Kushwaha <deepanshkushwaha9@gmail.com>
2022-08-24 13:10:32 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// console.log("it works");
+// const hamburger = document.querySelector('#ham')
+// const menus = document.querySelector("#menu")
+
+// hamburger.addEventListener("click", () => {
+//     menus.classList.toggle('hidden');
+//     })
